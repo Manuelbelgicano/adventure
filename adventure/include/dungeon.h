bd910c3f5534112ac57051583a62dff06484fd80
ManuelBelgicano <manuelgbal1@gmail.com>
2018-11-15 00:21:27 +0100

    Voy añadiendo los nuevos makefiles y otros archivos

--- adventure/include/dungeon.h
+++ adventure/include/dungeon.h
@@ -9,10 +9,35 @@
 #ifndef _DUNGEON_H
 #define _DUNGEON_H
 
+/**
+ * @brief Comienza la semilla de números aleatorios
+ */
 void start();
+/**
+ * @brief Describe un enfrentamiento con un trasgo
+ * @param g Trasgo al que se enfrenta el usuario. ES MODIFICADO
+ * @return El número de monedas ganadas en el enfrentamiento
+ */
 int event(Goblin &g);
+/** 
+ * @brief Describe una aventura por la mazmorra
+ * @param coins Monedas ganadas por el usuario durante la aventura. ES MODIFICADO
+ * @return @retval true Si el usuario está vivo
+ * 	   @retval false Si el usuario ha muerto
+ */
 bool walk(int &coins);
+/**
+ * @brief Calcula el daño de un golpe
+ * @return El daño del golpe
+ */
 int punch();
+/**
+ * @brief Crea un evento aleatorio
+ * @brief dead Si el usuario está muerto. ES MODIFICADO
+ * @param goblin Si el usuario se enfrenta a un trasgo. ES MODIFICADO
+ * @param coins Las monedas ganadas por el usuario
+ * @return El evento creado
+ */
 std::string happens(bool &dead,bool &goblin,int &coins);
 
 #endif
--- adventure/include/evil.h
+++ adventure/include/evil.h
@@ -7,15 +7,38 @@
 
 #ifndef _EVIL_H
 #define _EVIL_H
-
+/**
+ * TDA Goblin
+ * El TDA Goblin representa a un trasgo, con su nombre y sus puntos de vida
+ */
 struct Goblin {
-	int hp;
-	std::string name;
+	int hp; ///> Puntos de vida del trasgo
+	std::string name; ///> Nombre del trasgo
 };
-
+/**
+ * @brief Crea un trasgo
+ * @param _name Nombre del trasgo
+ * @param _hp Puntos de vida del trasgo
+ * @return El trasgo creado
+ */
 Goblin goblin(std::string _name,int _hp);
+/**
+ * @brief Comprueba si un trasgo está muerto
+ * @param g Trasgo del que se quiere comprobar su estado
+ * @return @retval true Si el trasgo está muerto (hp<=0)
+ * 	   @retval false Si el trasgo sigue vivo
+ */
 bool died(const Goblin &g);
+/**
+ * @brief Determina si un golpe acierta en el trasgo
+ * @param g Trasgo al que se le golpea. ES MODIFICADO
+ * @param dmg Daño que se le hace al goblin si es golpeado
+ */
 void hit(Goblin &g,int dmg);
+/**
+ * @brief Crea un trasgo aleatorio
+ * @return El trasgo creado
+ */
 Goblin randomise();
 
 #endif
